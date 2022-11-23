b1e65bdb66f7225c8ed17dc864d65f6951b02f54
tgarcia <tgarcia@osmozis.com>
2022-11-23 15:32:51 +0100

    Fix distance algorithm
    refs #2

--- game/tools.cpp
+++ game/tools.cpp
@@ -5,65 +5,50 @@
 
 
 
-uint32_t DistanceDeDamerauLevenshtein(QString reference, QString candidat)
+
+
+uint32_t DistanceDeDamerauLevenshtein(QByteArray reference, QByteArray candidat)
 {
     int i = 0;
     int j = 0;
-    int cout = 0;
-    int sum = 0;
-    int d[reference.size()][candidat.size()];
+    int coutSubstitution = 0;
+    int a = reference.size()+1;
+    int b = candidat.size()+1;
+    int d[a][b];
 
-    for (i = 0; i < reference.size() ; i++)
+    for (i = 0; i < a ; i++) {
+        d[i][0] = i;
+    }
+    for (j = 0; j < b ; j++) {
+        d[0][j] = j;
+    }
+
+    for ( j = 1 ; j < b ; j++)
     {
-        for ( j = 0 ; j < candidat.size() ; j++)
+        for (i = 1; i < a; i++)
         {
             if (reference.at(i) == candidat.at(j)){
-
+                coutSubstitution = 0;
             }
             else {
-                cout++;
+                coutSubstitution = 1;
             }
-            if ( (i>1) && (j>1) ) {                     // transposition
-                if ( (reference.at(i) == candidat.at(j-1))
-                     && (reference.at(i-1) == candidat.at(j)) ) {
 
-                }
+            d[i][j] = qMin(
+                        d[i-1][j] + 1 ,
+                  qMin( d[i][j-1] + 1 ,
+                        d[i-1][j-1] + coutSubstitution ));
+
+
+            // transposition
+            if ( (i > 1 && j > 1)
+                 && (reference.at(i) == candidat.at(j-1))
+                 && (reference.at(i-1) == candidat.at(j)) ) {
+                d[i][j] = qMin( d[i][j] , d[i-2][j-2] + coutSubstitution);
             }
         }
     }
-    return 0;
+    return d[a-1][b-1];
 }
-
-/*
-entier DistanceDeDamerauLevenshtein(caractere chaine1[1..longueurChaine1],
-                                    caractere chaine2[1..longueurChaine2])
-   // d est un tableau de longueurChaine1+1 rangées et longueurChaine2+1 colonnes
-   // d est indexé à partir de 0, les chaînes à partir de 1
-   déclarer entier d[0..longueurChaine1, 0..longueurChaine2]
-   // i et j itèrent sur chaine1 et chaine2
-   déclarer entier i, j, coûtSubstitution
-
-   pour i de 0 à longueurChaine1
-       d[i, 0] := i
-   pour j de 0 à longueurChaine2
-       d[0, j] := j
-
-   pour i de 1 à longueurChaine1
-      pour j de 1 à longueurChaine2
-          si chaine1[i] = chaine2[j] alors coûtSubstitution := 0
-                                     sinon coûtSubstitution := 1
-           d[i, j] := minimum(
-                                d[i-1, j  ] + 1,                 // effacement du nouveau caractère de chaine1
-                                d[i,   j-1] + 1,                 // insertion dans chaine1 du nouveau caractère de chaine2
-                                d[i-1, j-1] + coûtSubstitution   // substitution
-                             )
-           si(i > 1 et j > 1 et chaine1[i] = chaine2[j-1] et chaine1[i-1] = chaine2[j]) alors
-               d[i, j] := minimum(
-                                d[i, j],
-                                d[i-2, j-2] + coûtSubstitution   // transposition
-                             )
-
-   renvoyer d[longueurChaine1, longueurChaine2]
-*/
 
 
